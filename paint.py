7bde2532d94a80abaa5ae05f3fc5a711458293a3
Javier Hernández <javierhernandez@MacBook-Air-de-Javier.local>
2020-10-28 12:21:09 -0600

    Added a new color and circle function

--- paint.py
+++ paint.py
@@ -22,9 +22,19 @@ def square(start, end):
 
     end_fill()
 
+# Función para el círculo.
 def circle(start, end):
     "Draw circle from start to end."
-    pass
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(360):
+        forward(math.sin(math.radians(1))*(math.sqrt((end.x - start.x)**2+(end.y - start.y)**2)))
+        left(1)
+
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -54,7 +64,6 @@ def triangle(start, end):
 
     end_fill()
     
-
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
@@ -81,6 +90,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+# Color nuevo
+onkey(lambda: color('pink'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
